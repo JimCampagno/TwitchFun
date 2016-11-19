335e728cd5a175d7c8bac7229caa8b6c082b0491
JimCampagno <jimcampagno@gmail.com>
2016-11-19 15:06:40 -0500

    Work

--- TwitchTheTatMan/ViewController.swift
+++ TwitchTheTatMan/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
     
@@ -15,17 +16,46 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Example of Getting Top Games
-        twitch.get(request: .topGames) { _ in }
         
+        
+        // Example of Getting Top Games
+        //        twitch.get(request: .topGames) { _ in }
+        
+        //        twitch.get(request: .searchStreams(query: ValidTwitchSearch("Tim")!)) { _ in }
+        
+//        twitch.get(request: .searchChannels(query: ValidTwitchSearch("franplayshalo")!)) { _ in }
         
         // Example of Getting Features Games
-        twitch.get(request: .featuredGames) { _ in }
+        //        twitch.get(request: .featuredGames) { _ in }
         
         // Example of Channel Search
-        let q = ValidTwitchSearch("TimTheTatMan")
-        guard let query = q else { return }
-        twitch.get(request: .searchChannels(query: query)) { _ in }
+        //        let q = ValidTwitchSearch("TimTheTatMan")
+        //        guard let query = q else { return }
+        //        twitch.get(request: .searchChannels(query: query)) { _ in }
     }
+    
+    @IBAction func play(_ sender: UIButton) {
+        
+        guard let url = URL(string: "http://player.twitch.tv/channel=franplayshalo") else {
+            print("Bad URL")
+            return
+        }
+        
+        //        https://www.twitch.tv/lauren07
+        
+        
+        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
+        let player = AVPlayer(url: url)
+        
+        // Create a new AVPlayerViewController and pass it a reference to the player.
+        let controller = AVPlayerViewController()
+        controller.player = player
+        
+        // Modally present the player and call the player's play() method when complete.
+        present(controller, animated: true) {
+            player.play()
+        }
+    }
+    
     
 }
